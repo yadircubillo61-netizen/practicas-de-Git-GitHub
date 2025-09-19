5f7fb00f16bc67fbe53f4571d130926ba4e4ac7e
Yadir Cubillo <yadircubillo61@gmail.com>
2025-09-19 15:45:41 -0600

    cree una constante con un objeto

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let archivo = "Este es un archivo js creado desde github";
+const objeto = {
+  nombre:"Yadir",
+  apellido:"Cubillo",
+  edad:19,
+  contactos: {
+    instagram:" yadircubillo",
+    email: "yadircubillo61@gmail.com",
+  },
+
+
+
+}
